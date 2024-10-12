8bae19ec3740341925648dbcacbc1a8aeffa1194
OscarAnderlind <anderlindoscar@gmail.com>
2024-10-13 00:02:53 +0200

    the spider looks at you, watch out

---
+++ Assets/LookTowardsPlayer.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookTowardsPlayer : MonoBehaviour
+{
+    [SerializeField] private GameObject spiderBody;
+
+    private Player player;
+
+    private void Start()
+    {
+        player = FindObjectOfType<Player>();
+    }
+
+    void Update()
+    {
+        transform.right = player.transform.position - transform.position;
+        transform.position = spiderBody.transform.position;
+    }
+}
