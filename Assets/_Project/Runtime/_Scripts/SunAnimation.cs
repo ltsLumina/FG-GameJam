82764e04c96a8bc3a375fa271cca4d6707fd8ae9
OscarAnderlind <anderlindoscar@gmail.com>
2024-10-12 12:14:47 +0200

    pushing sunLeg tag

--- Assets/_Project/Runtime/_Scripts/SunAnimation.cs
+++ Assets/_Project/Runtime/_Scripts/SunAnimation.cs
@@ -11,9 +11,17 @@ public class SunAnimation : MonoBehaviour
 
     [SerializeField] int legAmount;
 
+    [SerializeField] int rayAmount;
+
     [SerializeField] List<LineRenderer> legs = new List<LineRenderer>();
 
-    [SerializeField] private LineRenderer sunLeg;
+    List<LineRenderer> unusedLegs = new List<LineRenderer>();
+
+    [SerializeField] LineRenderer sunLeg;
+
+    [SerializeField] Transform legManager;
+
+    Vector3 lastPos;
 
 
     private void Start()
@@ -21,7 +29,8 @@ public class SunAnimation : MonoBehaviour
 
         for(int i = 0; i < legAmount; i++)
         {
-            legs.Add(Instantiate(sunLeg, transform));
+            legs.Add(Instantiate(sunLeg, legManager));
+            FindNewAnchor(legs[i]);
         }
 
     }
@@ -29,38 +38,65 @@ public class SunAnimation : MonoBehaviour
     private void FixedUpdate()
     {
 
-        for (int i = 0; i < legAmount; i++)
+        Vector3 velocity;
+
+        velocity = (transform.position - lastPos).normalized;
+        lastPos = transform.position;
+
+        Debug.Log(velocity);
+
+        var newAngle = Vector3.SignedAngle(velocity, Vector3.right, Vector3.up);
+
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, newAngle));
+
+        for (int i = 0; i < rayAmount; i++)
         {
 
-            float rad = 360 / legAmount * Mathf.Deg2Rad * i;
+            float rad = 180 / rayAmount * Mathf.Deg2Rad * i;
 
-            Vector2 newDir = new Vector2(Mathf.Cos(rad), Mathf.Sin(rad));
+            Vector2 newDir = new Vector2(Mathf.Cos(rad), Mathf.Sin(rad) * transform.rotation.z);
 
             RaycastHit2D hit = Physics2D.Raycast(transform.position, newDir, legLenght, layer);
 
             if (hit.collider != null)
             {
                 Debug.DrawRay(transform.position, newDir, Color.green);
+                if(unusedLegs.Count > 0)
+                {                 
+                    unusedLegs[0].gameObject.SetActive(true);
+                    unusedLegs[0].gameObject.transform.position = hit.point;
+                    unusedLegs.Remove(unusedLegs[0]);
+                }
             }
             else
             {
                 Debug.DrawRay(transform.position, newDir, Color.red);
             }
+        }
 
-
+        for(int i = 0; i < legAmount; i++)
+        {
             for (int j = 0; j < 2; j++)
             {
                 legs[i].SetPosition(0, transform.position);
                 legs[i].SetPosition(1, legs[i].transform.position);
             }
-
-
-
         }
     }
 
-    private void OnTriggerExit2D(Collider2D collision)
+
+    private void FindNewAnchor(LineRenderer aLeg)
     {
+        aLeg.gameObject.SetActive(false);
+        unusedLegs.Add(aLeg);
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("sunLeg"))
+        {
+            FindNewAnchor(other.GetComponent<LineRenderer>());
+        }
         
     }
 
